14d5bd1ba8193a7ea17bb0f5eb9c562e1c5b0bf6
chenbiao <149027310@qq.com>
2024-03-29 09:29:59 +0800

    init

--- tessocr.go
+++ tessocr.go
@@ -1,12 +1,46 @@
 package dgtessocr
 
 import (
+	"errors"
 	dgctx "github.com/darwinOrg/go-common/context"
+	dgerr "github.com/darwinOrg/go-common/enums/error"
+	dghttp "github.com/darwinOrg/go-httpclient"
 	dglogger "github.com/darwinOrg/go-logger"
 	"github.com/otiai10/gosseract/v2"
+	"os"
 )
 
-func Ocr(ctx *dgctx.DgContext, imgFile string, languages ...string) (string, error) {
+var emptyImageError = errors.New("图片内容为空")
+
+func OcrImageUrl(ctx *dgctx.DgContext, imageUrl string, languages ...string) (string, error) {
+	imageBytes, err := dghttp.Client11.DoGet(ctx, imageUrl, nil, nil)
+	if err != nil {
+		dglogger.Errorf(ctx, "dghttp.Client11.DoGet error: %v", err)
+		return "", err
+	}
+	if len(imageBytes) == 0 {
+		dglogger.Errorf(ctx, "获取图片失败: %s", imageUrl)
+		return "", dgerr.SYSTEM_ERROR
+	}
+
+	return OcrImageBytes(ctx, imageBytes, languages...)
+}
+
+func OcrImageFile(ctx *dgctx.DgContext, imageFile string, languages ...string) (string, error) {
+	imageBytes, err := os.ReadFile(imageFile)
+	if err != nil {
+		dglogger.Errorf(ctx, "Failed to read image file: %s, error: %v", imageFile, err)
+		return "", err
+	}
+
+	return OcrImageBytes(ctx, imageBytes, languages...)
+}
+
+func OcrImageBytes(ctx *dgctx.DgContext, imageBytes []byte, languages ...string) (string, error) {
+	if len(imageBytes) == 0 {
+		return "", emptyImageError
+	}
+
 	// 创建一个客户端实例
 	client := gosseract.NewClient()
 
@@ -25,10 +59,10 @@ func Ocr(ctx *dgctx.DgContext, imgFile string, languages ...string) (string, err
 		return "", err
 	}
 
-	// 读取图像文件
-	err = client.SetImage(imgFile)
+	// 读取图像字节
+	err = client.SetImageFromBytes(imageBytes)
 	if err != nil {
-		dglogger.Errorf(ctx, "Failed to read image file: %v", err)
+		dglogger.Errorf(ctx, "Failed to read image bytes: %v", err)
 		return "", err
 	}
 
--- tessocr_test.go
+++ tessocr_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestOcr(t *testing.T) {
 	ctx := &dgctx.DgContext{TraceId: "123"}
-	text, err := dgtessocr.Ocr(ctx, "test.jpg", "chi_sim")
+	text, err := dgtessocr.OcrImageFile(ctx, "test.jpg", "chi_sim")
 	if err != nil {
 		panic(err)
 	}
