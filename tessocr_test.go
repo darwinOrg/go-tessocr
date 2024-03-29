c85f9b5b2182da04edb55fac335490496ccd4e81
chenbiao <149027310@qq.com>
2024-03-29 09:30:16 +0800

    init

--- tessocr_test.go
+++ tessocr_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestOcr(t *testing.T) {
+func TestOcrImageFile(t *testing.T) {
 	ctx := &dgctx.DgContext{TraceId: "123"}
 	text, err := dgtessocr.OcrImageFile(ctx, "test.jpg", "chi_sim")
 	if err != nil {
